e09d73be4e3213a5c1b8cef5e6ae4d9ce12ba9b4
AhmedEssamECE <ahmedelqmd1@gmail.com>
2025-04-23 03:20:55 +0200

    asaw

---
+++ IEEE_SSCS_SWE/c_tasks/task-6-compiler.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int j = 0;
+    int x = 3;
+    printf("hello world");
+    if (j == 1)
+    {
+        printnothing("error");
+    }
+    else
+    {
+        printf("hello world \n");
+    }
+}
