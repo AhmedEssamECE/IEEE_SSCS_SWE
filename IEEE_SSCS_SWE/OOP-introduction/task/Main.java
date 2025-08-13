135d3da0cfe8b717b9a1a7980b9293015e8e306d
AhmedEssamECE <manazhgt@gmail.com>
2025-08-13 22:28:29 +0300

    Update Main.java

--- IEEE_SSCS_SWE/OOP-introduction/task/Main.java
+++ IEEE_SSCS_SWE/OOP-introduction/task/Main.java
@@ -1,19 +1,22 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student("Alice", 20);
-        Student s2 = new Student("Bob", 25);
-        Student s3 = new Student("Charlie", 22);
 
-        // Save to file
-        s1.saveToFile();
-        s2.saveToFile();
-        s3.saveToFile();
+        Course c1 = new Course("Math", 88.5);
+        Course c2 = new Course("History", 92.0);
+        Course c3 = new Course("Science", 75.0);
 
-        // Search by name
-        List<Student> allStudents = Arrays.asList(s1, s2, s3);
-        List<Student> results = Student.searchByName(allStudents, "al");
-        System.out.println("Search results: " + results);
+        s1.addCourse(c1);
+        s1.addCourse(c2);
+        s1.addCourse(c3);
+
+        System.out.println(s1);
+        System.out.println("Average grade: " + s1.getAverageGrade());
+
+        s1.sortCoursesByGrade();
+        System.out.println("Courses sorted by grade: " + s1.getCourses());
+
+        s1.removeCourse("History");
+        System.out.println("After removing History: " + s1.getCourses());
     }
 }
